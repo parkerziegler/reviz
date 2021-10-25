6880d3dbd9b344d1abeee4ca99fabfbe5df52bb4
Logan Caraco <lbc.dna@gmail.com>
2021-10-25 15:03:00 -0700

    auto-format

--- src/walk.ts
+++ src/walk.ts
@@ -1,19 +1,19 @@
 export function walk(subtree: Node): Node[] {
-    const treeWalker = document.createTreeWalker(
-      subtree,
-      NodeFilter.SHOW_ELEMENT,
-      {
-        acceptNode: () => NodeFilter.FILTER_ACCEPT
-      },
-    );
-  
-    const nodeList: Node[] = [];
-    let currentNode: Node | null = treeWalker.currentNode;
-  
-    while (currentNode) {
-      nodeList.push(currentNode);
-      currentNode = treeWalker.nextNode();
+  const treeWalker = document.createTreeWalker(
+    subtree,
+    NodeFilter.SHOW_ELEMENT,
+    {
+      acceptNode: () => NodeFilter.FILTER_ACCEPT,
     }
-  
-    return nodeList;
+  );
+
+  const nodeList: Node[] = [];
+  let currentNode: Node | null = treeWalker.currentNode;
+
+  while (currentNode) {
+    nodeList.push(currentNode);
+    currentNode = treeWalker.nextNode();
   }
+
+  return nodeList;
+}
