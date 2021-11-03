708283052079806d70dd7d6fb4daa2dad1b5a8e4
Logan Caraco <lbc.dna@gmail.com>
2021-11-03 09:30:55 -0700

    uncommented plotting stuff

--- pages/examples/bar.tsx
+++ pages/examples/bar.tsx
@@ -2,7 +2,7 @@ import * as fs from 'fs';
 import * as path from 'path';
 
 import * as React from 'react';
-// import * as Plot from '@observablehq/plot';
+import * as Plot from '@observablehq/plot';
 import Head from 'next/head';
 
 interface Letter {
@@ -21,30 +21,28 @@ const Histogram: React.FC<Props> = ({ data }) => {
     // eslint-disable-next-line no-console
     console.log(data.length);
     
-    // const data = new Array(1000).fill(0).map((_, i) => collatz(i + 1, 0));
+    const plot = Plot.plot({
+      x: {
+        label: 'Stopping Time',
+      },
+      y: {
+        label: 'Frequency',
+        grid: true,
+      },
+      marks: [
+        Plot.rectY(data, {
+          ...Plot.binX({ y: 'count' }, { x: (d: number) => d }),
+          fill: 'steelblue',
+        }),
+        Plot.ruleY([0]),
+      ],
+    });
 
-    // const plot = Plot.plot({
-    //   x: {
-    //     label: 'Stopping Time',
-    //   },
-    //   y: {
-    //     label: 'Frequency',
-    //     grid: true,
-    //   },
-    //   marks: [
-    //     Plot.rectY(data, {
-    //       ...Plot.binX({ y: 'count' }, { x: (d: number) => d }),
-    //       fill: 'steelblue',
-    //     }),
-    //     Plot.ruleY([0]),
-    //   ],
-    // });
+    root.current.appendChild(plot);
 
-    // root.current.appendChild(plot);
-
-    // return (): void => {
-    //   root.current.removeChild(plot);
-    // };
+    return (): void => {
+      root.current.removeChild(plot);
+    };
   }, []);
 
   return (
