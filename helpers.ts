29e7e00859bc6f703fea345255808d8fd818b79d
Logan Caraco <lbc.dna@gmail.com>
2021-11-04 09:49:18 -0700

    naming fix

--- helpers.ts
+++ helpers.ts
@@ -1,9 +1,9 @@
 import * as fs from 'fs';
 import * as path from 'path';
 
-export function readData<T extends object>(json_name: string): T[] {
+export function readData<T extends object>(jsonName: string): T[] {
   const json = fs
-    .readFileSync(path.join(process.cwd(), `data/${json_name}.json`))
+    .readFileSync(path.join(process.cwd(), `data/${jsonName}.json`))
     .toString();
   return JSON.parse(json) as T[];
 }
