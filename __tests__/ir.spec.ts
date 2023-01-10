cc17ba77d9dd803161798458cf810075d3ce95d5
parkerziegler <parkerellisziegler@gmail.com>
2023-01-09 19:49:13 -0800

    test: add testing infrastructure and initial tests for ir.ts

---
+++ __tests__/ir.spec.ts
@@ -0,0 +1,76 @@
+import { buildVizSpec } from '../src/ir';
+
+import { generatePointDataset, defaultPresAttrs } from './test-utils';
+
+describe('IR', () => {
+  describe('buildVizSpec', () => {
+    describe('Scatterplot', () => {
+      it(`should infer a Scatterplot for charts with:
+      1. markType === 'circle,
+      2. A consistent radius attribute for all marks,
+      3. Sibling elements with any variance in cy attributes
+    `, () => {
+        // Generate a semi-random scatterplot dataset.
+        const points = generatePointDataset(100);
+
+        const vizAttrs = {
+          markType: 'circle' as const,
+          xScaleType: 'continuous' as const,
+          geomAttrs: new Map<string, Set<string>>([
+            ['cx', new Set(points.map((d) => d.cx.toString()))],
+            ['cy', new Set(points.map((d) => d.cy.toString()))],
+            ['r', new Set([points[0].r.toString()])],
+          ]),
+          presAttrs: new Map(),
+          positionAttrs: points.map((d) => {
+            return {
+              type: 'circle' as const,
+              cy: d.cy.toString(),
+            };
+          }),
+        };
+
+        expect(buildVizSpec(vizAttrs)).toEqual({
+          type: 'Scatterplot',
+          r: +points[0].r,
+          ...defaultPresAttrs,
+        });
+      });
+    });
+
+    describe('StripPlot', () => {
+      it(`should infer a StripPlot for charts with:
+        1. markType === 'circle',
+        2. A consistent radius attribute for all marks,
+        3. Sibling elements with consistent cy attributes
+      `, () => {
+        // Generate a semi-random strip plot dataset.
+        // Bin points into "lanes" to simulate the placement of points in a strip plot.
+        const points = generatePointDataset(100, { createLanes: true });
+
+        const vizAttrs = {
+          markType: 'circle' as const,
+          xScaleType: 'continuous' as const,
+          geomAttrs: new Map<string, Set<string>>([
+            ['cx', new Set(points.map((d) => d.cx.toString()))],
+            ['cy', new Set(points.map((d) => d.cy.toString()))],
+            ['r', new Set([points[0].r.toString()])],
+          ]),
+          presAttrs: new Map(),
+          positionAttrs: points.map((d) => {
+            return {
+              type: 'circle' as const,
+              cy: d.cy.toString(),
+            };
+          }),
+        };
+
+        expect(buildVizSpec(vizAttrs)).toEqual({
+          type: 'StripPlot',
+          r: +points[0].r,
+          ...defaultPresAttrs,
+        });
+      });
+    });
+  });
+});
--- __tests__/test-utils.ts
+++ __tests__/test-utils.ts
@@ -1,5 +1,70 @@
+import chunk from 'lodash.chunk';
+
+/**
+ * Generate a random character from A-Z using ASCII codes.
+ *
+ * @returns – a random character in the set {A...Z}.
+ */
 export const getRandChar = (): string =>
   String.fromCharCode(65 + Math.floor(Math.random() * 26));
 
+/**
+ * Generate a random integer in the range [0, max).
+ *
+ * @param max - the upper bound of the random integer to generate.
+ * @returns - a random integer in the range [0, max).
+ */
 export const getRandInt = (max = 1000): number =>
   Math.floor(Math.random() * max);
+
+interface Point {
+  cx: string;
+  cy: string;
+  r: string;
+}
+
+export const generatePointDataset = (
+  max = 1000,
+  { createLanes } = { createLanes: false }
+): Point[] => {
+  // Generate a semi-random point dataset.
+  const points = new Array(getRandInt(max)).fill(undefined).map(() => {
+    return {
+      cx: getRandInt().toString(),
+      cy: getRandInt().toString(),
+      r: getRandInt().toString(),
+    };
+  });
+
+  // If creating lanes, partition the dataset into n < max lanes.
+  // For each point in a given lane, ensure its cy attribute is
+  // consistent with its siblings.
+  if (createLanes) {
+    // Generate fewer lanes than there are points by dividing max by 4.
+    const lanes = chunk(points, getRandInt(max / 4));
+    const strips = lanes.flatMap((lane) => {
+      // All points in a lane should use the same cy.
+      const cy = getRandInt().toString();
+
+      return lane.map((point) => {
+        return {
+          ...point,
+          cy,
+        };
+      });
+    });
+
+    return strips;
+  }
+
+  return points;
+};
+
+// Default presentational attributes for tests.
+export const defaultPresAttrs = {
+  fill: [],
+  'fill-opacity': [],
+  stroke: [],
+  'stroke-opacity': [],
+  'stroke-width': [],
+};
