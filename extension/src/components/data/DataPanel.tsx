2e0168e326f3c9e6b6b650a53f232426a3e55227
parkerziegler <parkerellisziegler@gmail.com>
2023-06-16 16:05:40 -0700

    fix: final PR review nits

--- extension/src/components/data/DataPanel.tsx
+++ extension/src/components/data/DataPanel.tsx
@@ -1,4 +1,3 @@
-import * as React from "react";
 import type { DSVRowArray } from "d3-dsv";
 
 import Heading from "../shared/Heading";
@@ -13,7 +12,7 @@ interface Props {
 
 const DataPanel: React.FC<Props> = ({ data, setData }) => {
   return (
-    <div className="stack stack-sm relative basis-1/3 px-3 py-2">
+    <div className="stack stack-sm basis-1/3 px-3 py-2">
       <Heading className="self-start">Data</Heading>
       {data ? <DataTable data={data} /> : <DataUpload setData={setData} />}
     </div>
